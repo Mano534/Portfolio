aa6f04bb5293e06fb8985ad27c4c9d08e60ddb26
monmonmon175@yahoo.com <monmonmon175@yahoo.com>
2020-10-01 21:20:40 +0200

    Updates

---
+++ precache-manifest.051286045b218f1aba98acaf62eb8ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869f67eb4dbc0a326fe6b04f348fe264",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "07b903a093c724d7a5b9",
+    "url": "/Portfolio/static/css/main.efba4093.chunk.css"
+  },
+  {
+    "revision": "e0e405eb248b1c3f7d3e",
+    "url": "/Portfolio/static/js/2.08aeebab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.08aeebab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b903a093c724d7a5b9",
+    "url": "/Portfolio/static/js/main.f717510f.chunk.js"
+  },
+  {
+    "revision": "855fcbd74e9c379b09ce",
+    "url": "/Portfolio/static/js/runtime-main.6c8fd76b.js"
+  }
+]);
